870802df0ee9b089740c370cfd6dbe7a210acd1e
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-13 03:15:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const writeCards1 =(["Ada", "Brendan", "Ali"], "birthday");function writeCards(array, string){
+    const newArray = []
+    for(let i = 0 ; i < array.length; i++){
+    newArray.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+    }
+    return newArray;
+}
+writeCards(writeCards1);
+
+
+let NUMBER = 10;
+function countDown(number){
+    while (number >= 0){
+    console.log(number--)
+    }
+}
+
+countDown(NUMBER)
