dc899c41fae9f68b96ba6101d4180926f2a5d65d
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-13 02:19:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solutions in this file
-const card = ["Ada", "Brendan", "Ali", "Lisa", "Kaitlin","Jan"];
+const card = [ "Lisa", "Kaitlin","Jan"];
 function writeCards(card)
 {
     for (let i = 0; i < card.length ; i++) {
