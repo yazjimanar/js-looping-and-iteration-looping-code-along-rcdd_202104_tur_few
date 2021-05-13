8af062f901a5ffad462cdb6300607d33e3de4af5
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-13 02:39:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const card1 =[] ;
 function writeCards(card)
 {
     for (let i = 0; i < card.length ; i++) {
-      card1.push (  `Thank you ${card[i]}, for the wonderful birthday gift!`);
+      card1.push (  console.log(`Thank you ${card[i]}, for the wonderful birthday gift!`));
           }
             //   return card;
               return card1;
