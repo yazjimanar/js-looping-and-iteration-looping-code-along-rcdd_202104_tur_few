9436f6704805ccefa19c608e338d5704e30aa92c
Mohammed Manar Yazji <yazjimanar92@gmail.com>
2021-05-13 02:38:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Code your solutions in this file
-const writeCards = (["Ada", "Brendan", "Ali"], "birthday");
+const writeCards1 = ["Ada", "Brendan", "Ali"];
 const card1 =[] ;
 function writeCards(card)
 {
     for (let i = 0; i < card.length ; i++) {
-      card1.push (  console.log(`Thank you ${card[i]}, for the wonderful birthday gift!`));
+      card1.push (  `Thank you ${card[i]}, for the wonderful birthday gift!`);
           }
-              return card;
+            //   return card;
+              return card1;
 
 }
 
-writeCards(card);
+writeCards(writeCards1);
